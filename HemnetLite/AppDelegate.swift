c8b1c6f05d53b5182e6a310038a7483dbd0381b5
Tetiana Bernatska <tetiana.bernatska@comhem.com>
2020-02-12 12:47:42 +0100

    Added an empty  project

---
+++ HemnetLite/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ HemnetLiteTests/HemnetLiteTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+@testable import HemnetLite
+
+class HemnetLiteTests: XCTestCase {}
